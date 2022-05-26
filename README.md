1f5c6f3b22808674a48ea2e0dd9f011e07818b76
nameless <satyamsharma2106@gmail.com>
2022-05-26 15:48:11 +0530

    add computerPlay function to select random answers

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock-paper-scissors
+this project is to practice some of the javscript fundamentals, such as functions, loops, conditionals, etc.
---
+++ script.js
@@ -0,0 +1,15 @@
+// make program to select random of the three possiblities
+function computerPlay() {
+    randomIndexNumber = floor(Math.random() * 2));
+    if (randomIndexNumber === 0) {
+        return "rock";
+    }
+    else if (randomIndexNumber === 1) {
+        return "paper";
+    }
+    return "scissors";
+}
+// make function to take the moves and declare the winner
+    // compare moves case insensitively
+    // return result in a formatted string
+//create function to loop over 5 games and print the result
