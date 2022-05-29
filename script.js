65336af3380413f2bd53179b78e5126e05a63e2a
nameless <satyamsharma2106@gmail.com>
2022-05-29 08:29:06 +0530

    add ui to the game

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function computerPlay() {
     return outcomeList[randomIndexNumber]; 
 }
 
+
+
 // make function to take the moves and declare the winner
     // return result in a formatted string
-
 function playRound(playerSelection, computerSelection) {
 
 // convert selections to lowercase to compare them
@@ -47,40 +48,54 @@ function playRound(playerSelection, computerSelection) {
 
 // create function to loop over 5 games and print the result
 
-function game() {
-    
-    let playerScore = 0;
-    let computerScore = 0;
+function game(playerSelection) {
 
-    for (let i = 0; i < 5; i++) {
+//    for (let i = 0; i < 5; i++) {
 
         // prompt user for input
-        const playerSelection = prompt();
+//        const playerSelection = getPlayerSelection();
         // generate computer move
-        const computerSelection = computerPlay();
+    const computerSelection = computerPlay();
         
 
         // print winner or loser of the round
-        console.log(playRound(playerSelection, computerSelection));
+    result.textContent = playRound(playerSelection, computerPlay());
 
         // give scores
 
-        if (playRound(playerSelection, computerSelection).includes("win")) {
-            playerScore++;
-        }
-        if (playRound(playerSelection, computerSelection).includes("lose")) {
-            computerScore++;
-        }
+    if (playRound(playerSelection, computerSelection).includes("win")) {
+        playerScore++;
     }
+    if (playRound(playerSelection, computerSelection).includes("lose")) {
+        computerScore++;
+    }
+//    }
     // print winner or loser of the game
-
-    if (playerScore > computerScore) {
-        console.log("Player won the game!");
+    scoreBoard.textContent = `Player score: ${playerScore} Computer score: ${computerScore}`;
+    if (playerScore === 5) {
+        result.textContent = "Player won the game!";
+        playerScore = 0;
+        computerScore = 0;
     }
-    else if (playerScore < computerScore) {
-        console.log("Computer won the game!");
+    else if (computerScore === 5) {
+        result.textContent = "Computer won the game!";
+        playerScore = 0;
+        computerScore = 0;
     }
-    else {
-        console.log("The game is a draw!");
-    }
+    //else {
+        //console.log("The game is a draw!");
+    //}
 }
+function onClick() {
+    game(this.className);
+
+}
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const scoreBoard = document.querySelector('.scoreboard');
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach(button => button.addEventListener('click', onClick));
+//game();
